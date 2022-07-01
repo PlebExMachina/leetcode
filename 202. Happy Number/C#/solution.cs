58a733bd2811986540803d3ffe158be9e8fc34ab
Kenneth Willeford <plebexmachina@gmail.com>
2022-07-01 01:24:22 -0700

    Invert Binary Tree C#

--- 202. Happy Number/C#/solution.cs
+++ 202. Happy Number/C#/solution.cs
@@ -1,23 +1,4 @@
 public class Solution {
-    // Loops occur on these numbers.
-    private static bool WillLoop(int n){
-        switch(n) {
-            case 4:
-            case 16:
-            case 20:
-            case 37:
-            case 42:
-            case 58:            
-            case 89:
-            case 145:
-                return true;
-                break;
-            default:
-                break;
-        }
-        return false;        
-    }
-    
     private static int SumOfSquares(int n){
         int output = 0;
         while(n != 0){
@@ -27,15 +8,15 @@ public class Solution {
         }
         return output;
     }
-    
+
+    // Cycle Finding Algorithm
     public bool IsHappy(int n) {
-        while(n != 1){
-            if(WillLoop(n)){
-                return false;
-            } else {
-                 n = SumOfSquares(n);   
-            }
+        int slow = n;
+        int fast = SumOfSquares(n);
+        while(fast != 1 && slow != fast){
+            slow = SumOfSquares(slow);
+            fast = SumOfSquares(SumOfSquares(fast));
         }
-        return n == 1;
+        return fast == 1;
     }
 }
